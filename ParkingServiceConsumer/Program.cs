c6fd2e8517c3291c8078aa1893d30577b98b0dff
Ciprian Prohozescu <ciprian.prohozescu@internetzoo.dk>
2021-10-04 11:22:58 +0200

    Add a .net core solution and console project, DEV-1

---
+++ ParkingServiceConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParkingServiceConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
