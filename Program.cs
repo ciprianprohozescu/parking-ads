e71d73eec6932d7cb179064f9db507b9811701bd
Andrei Mataoanu <amatao1999@gmail.com>
2021-10-06 13:08:59 +0200

    Kinda worked? Im still confused tho.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace parking_ads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
