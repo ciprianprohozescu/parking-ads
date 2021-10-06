d5203d605289583a0441711cc27de5f4eeb991f1
thomas <tomifaludi@gmail.com>
2021-10-06 13:08:15 +0200

    Ad consumer and test for it.

---
+++ AdServiceConsumer/Consumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RestSharp;
+
+namespace AdServiceConsumer
+{
+    public class Consumer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static IRestResponse Consume()
+        {
+            var client = new RestClient("http://psuaddservice.fenris.ucn.dk");
+            var request = new RestRequest("service", DataFormat.Json);
+            var response = client.Get(request);
+            return response;
+        }
+    }
+}
---
+++ AdServiceConsumerTest/ConsumerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using RestSharp;
+using AdServiceConsumer;
+
+namespace AdServiceConsumerTest
+{
+    public class ConsumerTest
+    {
+        [Fact]
+        public void ConsumeTest()
+        {
+            IRestResponse response = Consumer.Consume();
+            Assert.Equal(200, (int)response.StatusCode);
+        }
+    }
+}
