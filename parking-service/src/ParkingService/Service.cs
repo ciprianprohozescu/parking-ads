11f77db275a45b29b744f3072907102669eb7fb5
Ciprian Prohozescu <ciprianprohozescu@gmail.com>
2021-11-29 17:12:45 +0100

    Send response back

--- parking-service/src/ParkingService/Service.cs
+++ parking-service/src/ParkingService/Service.cs
@@ -13,22 +13,39 @@ namespace ParkingService
             var factory = new ConnectionFactory() { HostName = "rabbitmq" };
         	var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
-			channel.QueueDeclare(queue: "parking-requests",
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
+			channel.QueueDeclare(queue: "parking-requests", 
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
 
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) =>
             {
+                // Decode the message
                 var body = ea.Body.ToArray();
+                var props = ea.BasicProperties;
+                var replyProps = channel.CreateBasicProperties();
+                replyProps.CorrelationId = props.CorrelationId;
                 var message = Encoding.UTF8.GetString(body);
                 Console.WriteLine(" [x] Received {0}", message);
+                
+                // Request a list of parking spots from the UCN API
+                var client = new RestClient("http://psuparkingservice.fenris.ucn.dk");
+                // Set a timeout of 5 seconds
+                client.Timeout = 5000;
+                var request = new RestRequest("service", DataFormat.Json);
+                var response = client.Get(request);
+                
+                // Send a response with the parking spots list
+                channel.BasicPublish(exchange: "", 
+                    routingKey: props.ReplyTo,
+                    basicProperties: replyProps, 
+                    body: Encoding.UTF8.GetBytes(response.Content));
             };
             channel.BasicConsume(queue: "parking-requests",
-                                 autoAck: true,
-                                 consumer: consumer);
+                autoAck: true,
+                consumer: consumer);
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
